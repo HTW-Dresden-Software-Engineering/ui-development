5b664452258eb892775ece68788f6157f783f2ed
Ruben-David Kraus <ruben-david.kraus@ottogroup.com>
2025-09-19 13:47:26 +0200

    feat: added groupname input

---
+++ ui-example/src/components/group/GroupNameInput.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="space-y-2">
+        <Label for="group-name">Group Name</Label>
+        <Input v-model="groupName" id="group-name" type="text" placeholder="Enter group name" />
+    </div>
+</template>
+<script setup lang="ts">
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label";
+import { ref } from "vue";
+
+const groupName = ref('');
+</script>
