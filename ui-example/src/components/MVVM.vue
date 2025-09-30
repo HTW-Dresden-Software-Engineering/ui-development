fa945bd7c0516eca55a35918d39769fd4deb04a9
Ruben-David Kraus <ruben-david.kraus@ottogroup.com>
2025-09-30 14:22:20 +0200

    feat: Mvvm component

---
+++ ui-example/src/components/MVVM.vue
@@ -0,0 +1,16 @@
+<!-- View -->
+<template>
+    <div>
+        <h1>MVVM</h1>
+        <h2>Hello, {{ name }}</h2>
+    </div>
+    <!-- View-Model -->
+    <input v-model="name" placeholder="Enter your name" />
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue"
+
+// Model
+const name = ref("")
+</script>
